f39a8a8385d603d10e6b720aff663bb66cff245f
Venkataram Sivaram <veskimo123@gmail.com>
2021-01-10 19:22:56 -0800

    Basic command interpretation through shell

--- os/os.ino
+++ os/os.ino
@@ -1,8 +1,26 @@
+#define MAX_CMD_LENGTH 1024
+
 void setup()
 {
   Serial.begin(9600);
   Serial.print("root$ ");
+
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
 }
+
+bool s = false;
+void set()
+{
+  if (s)
+    digitalWrite(13, HIGH);
+  else
+    digitalWrite(13, LOW);
+  
+  s = !s;
+}
+
+char cmd[MAX_CMD_LENGTH];
 
 void loop()
 {
@@ -11,6 +29,7 @@ void loop()
     case 0:
       break;
     case 1:
+      execute();
       Serial.print("root$ ");
       break;
     default:
@@ -19,18 +38,68 @@ void loop()
   }
 }
 
+char exe[MAX_CMD_LENGTH];
+
+void execute()
+{
+  Serial.print("cmd = \"");
+  Serial.print(cmd);
+  Serial.println("\"");
+
+  int i = 0;
+  while (i < MAX_CMD_LENGTH && cmd[i] != 0) {
+    if (isspace(cmd[i])) {
+      Serial.print("exe = \"");
+      Serial.print(exe);
+      Serial.println("\"");
+
+      if (strcmp(exe, "set") == 0) {
+        Serial.println("Set command detected");
+      } else {
+        Serial.println("Command not found.");
+      }
+    } else {
+      exe[i] = cmd[i];
+    }
+
+    i++;
+  }
+
+  if (strcmp(exe, "set") == 0) {
+    Serial.println("Set command detected");
+
+    set();
+  } else {
+    Serial.println("Command not found.");
+  }
+
+  memset(cmd, 0, MAX_CMD_LENGTH);
+  memset(exe, 0, MAX_CMD_LENGTH);
+}
+
+int pos = 0;
+
 int process(char c)
 {
   switch (c) {
   case 13:
     Serial.println();
+    
+    pos = 0;
     return 1;
   case 127:
-    Serial.write('\b');
-    Serial.write(' ');
-    Serial.write('\b');
+    if (pos) {
+      cmd[pos--] = 0;
+      
+      Serial.write('\b');
+      Serial.write(' ');
+      Serial.write('\b');
+    }
+    
     break;
   default:
+    cmd[pos++] = c;
+    
     if (!isprint(c)) {
       Serial.print("[");
       Serial.print((byte) c);
